cb3f9504680f0d7d1d676003bc17820f43f24121
sam <woshidengshangfu@163.com>
2026-08-28 01:51:20 +0800

    refactor: expose agent transport negotiation

--- src/tracecite/integrations/__init__.py
+++ src/tracecite/integrations/__init__.py
@@ -1,5 +1,12 @@
 """Adapters exposing TraceCite Runtime to external Agent hosts."""
 
+from .agent_profile import (
+    AgentCapabilities,
+    AgentProfile,
+    get_agent_profile,
+    profile_names,
+    select_agent_profile,
+)
 from .agent_projection import prefer_smaller_agent_view
 from .context_engine import (
     CONTEXT_SCHEMA_VERSION,
@@ -10,10 +17,15 @@ from .context_engine import (
 )
 
 __all__ = [
+    "AgentCapabilities",
+    "AgentProfile",
     "CONTEXT_SCHEMA_VERSION",
     "ContextEngine",
     "ContextState",
     "ContextStateStore",
+    "get_agent_profile",
     "prefer_smaller_agent_view",
+    "profile_names",
     "project_search_delta",
+    "select_agent_profile",
 ]
